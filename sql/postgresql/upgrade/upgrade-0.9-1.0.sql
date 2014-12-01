30df55fb5a46dd17ab010d7c1a07d3877afac9f1
Malte Sussdorff <malte.sussdorff@cognovis.com>
2014-12-01 16:19:59 +0100

    Added the ability to view the absence type in the info component (266)

--- sql/postgresql/upgrade/upgrade-0.9-1.0.sql
+++ sql/postgresql/upgrade/upgrade-0.9-1.0.sql
@@ -2,3 +2,6 @@ SELECT acs_log__debug('/packages/intranet-cust-nts/sql/postgresql/upgrade/upgrad
 
 -- Fix a problem preventing form HR disapproval to work
 update wf_arcs set guard_custom_arg = 'hr_review_reject_p' where workflow_key = 'hr_vacation_approval_wf' and transition_key = 'hr_approve' and place_key = 'hr_approved';
+
+insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name)
+values (28105,281,'Type','[im_category_from_id $absence_type_id]',6,'absence_type_id');
