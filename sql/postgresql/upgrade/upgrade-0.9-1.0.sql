99523dc25547ae98867ee1d2a6a1fa823acb7c9d
Malte Sussdorff <malte.sussdorff@cognovis.de>
2014-12-10 14:40:16 +0100

    fixed absence list details when colors should be hidden (318)

--- sql/postgresql/upgrade/upgrade-0.9-1.0.sql
+++ sql/postgresql/upgrade/upgrade-0.9-1.0.sql
@@ -5,3 +5,5 @@ update wf_arcs set guard_custom_arg = 'hr_review_reject_p' where workflow_key = 
 
 insert into im_view_columns (column_id, view_id, column_name, column_render_tcl, sort_order,variable_name)
 values (28105,281,'Type','[im_category_from_id $absence_type_id]',6,'absence_type_id');
+
+update im_view_columns set visible_for = 'if {$hide_colors_p eq 0} {set visible_p 1} else {set visible_p 0}' where column_id in (20007,20009,1066);
