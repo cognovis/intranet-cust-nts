1ea92f1d9f4ec25b4b1b3e4c64609e69dd06d374
Malte Sussdorff <malte.sussdorff@cognovis.com>
2015-03-27 17:10:31 +0100

    Disable edit button for HR
    Signed-off-by: Malte Sussdorff <malte.sussdorff@cognovis.com>

---
+++ sql/postgresql/upgrade/upgrade-1.0.0.0.2-1.0.0.0.3.sql
@@ -0,0 +1,10 @@
+SELECT acs_log__debug('/packages/intranet-cust-nts/sql/postgresql/upgrade/upgrade-1.0.0.0.2-1.0.0.0.3.sql','');
+
+update apm_parameter_values
+set attr_value = 'im_nts_absence_new_page_wf_perm_edit_button'
+where parameter_id in (
+        select parameter_id
+        from apm_parameters
+        where   parameter_name = 'AbsenceNewPageWfEditButtonPerm' and
+                package_id = '12195'
+        );
